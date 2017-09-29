a7eb25e0e0b65606cf4ce9f232e7b7639b371559
Rylan Boothman <boothmanrylan@gmail.com>
2017-09-29 16:37:08 -0600

    Refactored to allow more flexibility with running form other scripts or the command line

--- kmer_prediction.py
+++ kmer_prediction.py
@@ -1,102 +1,330 @@
-from kpal.klib import Profile
-import h5py
-import numpy as np
-import os
+import subprocess
 from sklearn import svm
-from random import randrange
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.model_selection import StratifiedShuffleSplit as ssSplit
+import os
+import lmdb
+import sys
 
+def start(filename, k, limit, txn, data):
+    """
+    Helper method for kmer_prediction.run(), should not be used on its own.
 
-human_file_path = '/home/james/Human/'
-bovine_file_path = '/home/james/Bovine/'
+    Performs a kmer count on filename, counting kmers witha length of k and
+    removing any kmer that has a count less than limit. Clears the database
+    data and then writes each kmer as a key with value -1 to data.
 
-def convert_fasta_to_kmer_count_array(file, k):
-    max_kmers = 4**k
-    f = open(file)
-    p = Profile.from_fasta(f, length=k, name='kmer')
-    f.close()
+    Returns a lexicographically sorted list of lists. Where each element in the
+    outer list has a first element that is a kmer and a second element that is
+    the count corresponding to that kmer.
+    """
+    args = ['jellyfish', 'count', '-m', '%d' % k, '-s', '10M', '-t', '30', '-C',
+            '%s' % filename, '-o', 'test.jf', '-L', '%d' % limit]
+    p = subprocess.Popen(args, bufsize=-1)
+    p.communicate()
+    # Get results from kmer count
+    args = ['jellyfish', 'dump', '-c', 'test.jf']
+    p = subprocess.Popen(args, bufsize=-1, stdout=subprocess.PIPE)
+    out, err = p.communicate()
+    # Transform results into usable format
+    arr = [x.split(' ') for x in out.split('\n') if x]
 
-    #Saves the kPAL 'Profile as a hdf5 file'
-    f = h5py.File("temp.hdf5", "w")
-    p.save(f, 'kmer')
-    f.close()
+    txn.drop(data)
+    for line in arr:
+        txn.put(line[0], line[1])
+    array = []
+    cursor = txn.cursor()
+    #array = [[key, val] for key, val in txn.cursor()]
+    for key, value in cursor:
+        array.append([key, value])
+        txn.put(key, '-1')
 
-    #Open the saved file using h5py
-    kmer_counts = h5py.File("temp.hdf5", "r")
+    return array
 
-    #Access the h5py group 'profiles'
-    grp = kmer_counts['profiles']
+def firstpass(filename, k, limit, txn):
+    """
+    Helper method for kmer_prediction.run(), should not be used on its own.
 
+    Performs a kmer count on filename, counting kmers with a length of k and
+    removing any kmer that has a count less than limit. Only saves kmers that
+    are already present in the database that txn points to. Removes any kmer
+    from the databse that is not present in filename.
 
-    dataset = grp.require_dataset('kmer', shape=(max_kmers,), dtype='int64', exact=False)
+    Returns a list with the same specifications as the list returned by
+    kmer_prediction.start()
+    """
+    args = ['jellyfish', 'count', '-m', '%d' % k, '-s', '10M', '-t', '30', '-C',
+            '%s' % filename, '-o', 'test.jf', '-L', '%d' % limit]
+    p = subprocess.Popen(args, bufsize=-1)
+    p.communicate()
+    # Get results from kmer count
+    args = ['jellyfish', 'dump', '-c', 'test.jf']
+    p = subprocess.Popen(args, bufsize=-1, stdout=subprocess.PIPE)
+    out, err = p.communicate()
+    # Transform results into usable format
+    arr = [x.split(' ') for x in out.split('\n') if x]
 
-    #Create np array to manipulate the dataset
-    arr = np.zeros((max_kmers,), dtype='int64')
+    for line in arr:
+        if txn.get(line[0], default=False):
+            txn.put(line[0], line[1], overwrite=True, dupdata=False)
 
-    #read the hdf5 file into the array
-    dataset.read_direct(arr, np.s_[0:max_kmers], np.s_[0:max_kmers])
-    kmer_counts.close()
-
-    return arr
-
-def generate_training_data(begin, end, k):
-    human_files = os.listdir(human_file_path)
-    human_files = [human_file_path + x for x in human_files]
-    bovine_files = os.listdir(bovine_file_path)
-    bovine_files = [bovine_file_path + x for x in bovine_files]
-    human_training = []
-    bovine_training = []
-    training_data = []
-    training_answers = []
-    size = end-begin+1
-    for i in range(size):
-        print "Creating training files #%d" % i
-        human_training.append(convert_fasta_to_kmer_count_array(human_files[i+begin], k))
-        bovine_training.append(convert_fasta_to_kmer_count_array(bovine_files[i+begin], k))
-    h_count = 0;
-    b_count = 0;
-    while h_count < size and b_count < size:
-        rand = randrange(0,10)
-        if rand%2 == 0:
-            training_data.append(human_training[h_count])
-            training_answers.append("Human")
-            h_count += 1
+    array = []
+    cursor = txn.cursor()
+    for key, value in cursor:
+        if value == '-1':
+            txn.delete(key)
         else:
-            training_data.append(bovine_training[b_count])
-            training_answers.append("Bovine")
-            b_count += 1
-    while h_count < size:
-        training_data.append(human_training[h_count])
-        training_answers.append("Human")
-        h_count += 1
-    while b_count < size:
-        training_data.append(bovine_training[b_count])
-        training_answers.append("Bovine")
-        b_count += 1
+            array.append([key, value])
+            txn.put(key, '-1')
 
-    return training_data, training_answers
+    return array
 
+def second_start(arr, k, txn, data):
+    """
+    Helper method for kmer_prediction.run(), should not be used on its own.
+
+    Clears the database data and then writes every kmer present in arr to the
+    database.
+
+    Returns a list of lexicographically sorted kmer counts. The kmers themselves
+    are not output, instead index 0 holds the count for the lexicographically
+    smallest kmer and index 1 holds the count for the next lexicagraphically
+    smallest kmer etc.
+    """
+    txn.drop(data)
+    for line in arr:
+        txn.put(line[0], line[1])
+    array = []
+    cursor = txn.cursor()
+    for key, value in cursor:
+        array.append(int(value))
+
+    return array
+
+def secondpass(arr, k, txn):
+    """
+    Helper method for kmer_prediction.run(), should not be used on its own.
+
+    Removes every kmer from arr that is not present in the database pointed to
+    by txn.
+
+    Returns a list of the same specifications as the list returned by
+    kmer_prediction.second_satart()
+    """
+    for line in arr:
+        if txn.get(str(line[0]), default=False):
+            txn.put(line[0], line[1], overwrite=True, dupdata=False)
+
+    array = []
+    cursor = txn.cursor()
+    for key, value in cursor:
+        array.append(int(value))
+
+    return array
+
+def print_status(counter, total):
+    """
+    Outputs a progress bar.
+    """
+    percent = (counter*100)/total
+    sys.stdout.write('\r')
+    sys.stdout.write("[%-44s] %d%%" % ('='*((percent*44)/100), percent))
+    sys.stdout.flush()
+
+def set_up_files(filepath):
+    """
+    Helper method for running kmer_prediction.py from the command line.
+
+    Takes a path to a directory, returns a list of the complete paths to each
+    file in the directory
+    """
+    if not filepath[-1] == '/':
+        filepath += '/'
+    return [filepath + x for x in os.listdir(filepath)]
+
+def setup_data(files, k, limit, txn, data):
+    """
+    Helper method for kmer_prediction.run(), should not be used on its own.
+
+    Takes a list of paths to fasta files, a kmer length, a lower limit on how
+    many times a kmer needs to occur in order for it to be output, and an lmdb
+    transaction and corresponding database.
+
+    Returns a list where each element of the list is a list of kmer counts for a
+    genome contained in one of the fasta files.
+    """
+    counter = 0
+    total = len(files)
+
+    arrays = []
+    arrays.append(start(files[0], k, limit, txn, data))
+    files.pop(0)
+    counter += 1
+
+    for filename in files:
+        print_status(counter, total)
+        arrays.append(firstpass(filename, k, limit, txn))
+        counter += 1
+
+    print_status(counter, total)
+    print ""
+
+    counter = 0
+    arrays[-1] = second_start(arrays[-1], k, txn, data)
+    counter += 1
+
+    i = len(arrays)-2
+    while i >= 0:
+        print_status(counter, total)
+        arrays[i] = secondpass(arrays[i], k, txn)
+        i-=1
+        counter += 1
+
+    print_status(counter, total)
+    print "\n"
+
+    return arrays
+
+def make_predictions(train_data, train_labels, test_data, test_labels):
+    """
+    Helper method for kmer_prediction.run(), should not be used on its own.
+
+    Takes training data and labels, and test data for an svm. If test_lables is
+    None returns predictions on test_data, other wise returns a percentage of
+    predictions that the svm got correct.
+    """
+    linear = svm.SVC(kernel='linear')
+    output = []
+
+    scaler = MinMaxScaler()
+    X = scaler.fit_transform(train_data)
+    Z = scaler.transform(test_data)
+
+    try:
+        linear.fit(X, train_labels)
+        if test_labels:
+            ans = linear.score(Z, test_labels)
+            return ans
+        else:
+            return linear.predict(Za)
+    except (ValueError, TypeError) as E:
+        print E
+        return -1
+
+def run(k, limit, num_splits, pos, neg, predict):
+    """
+    Uses kmer counts of genomes as input to a support vector machine to predict
+    presence or absence of a phenotype in a genome.
+
+    Parameters:
+        k:          The length of kmer to input
+        l:          A lower limit on how many times a kmer needs to be present in a
+                    genome in order to be ouput.
+        num_splits: How many times to train and test the model. Ignored if
+                    predict is not None.
+        pos:          Path to a directory containing fasta files for genomes that
+                    are positive for a phenotype.
+        neg:        Path to a directory containing fasta files for genomes that
+                    are negative for a phenotype.
+        predict:    Path to a directory containing fasta files for genomes that
+                    you want to perdict whether or not they have a phenotype.
+                    Can be None, if None the files in pos and neg will be
+                    shuffled and split so that 80%% are used to train the model
+                    and 20%% to test a model.
+    Returns:
+        Predict is None:        Percentage of predictions that the model got
+                                right. If num_splits greater than 1, this is the
+                                average of all runs.
+        Predict is not None:    Returns a binary a array, where 1 means the
+                                genome belongs to the pos group and 0 means the
+                                genome belongs to the neg group.
+    """
+
+    env = lmdb.open('database', map_size=int(2e9))
+
+    data = env.open_db(dupsort=False)
+
+    with env.begin(write=True, db=data) as txn:
+
+        if not predict:
+            files = pos + neg
+        else:
+            files = pos + neg + predict
+
+        arrays = setup_data(files, k, limit, txn, data)
+
+        labels=[1 for x in range(len(pos))]+[0 for x in range(len(neg))]
+
+        if not predict:
+            sss = ssSplit(n_splits=num_splits, test_size=0.2, random_state=42)
+
+            avg = 0.0
+
+            for indices in sss.split(arrays, labels):
+                X = [arrays[x] for x in indices[0]]
+                Y = [labels[x] for x in indices[0]]
+                Z = [arrays[x] for x in indices[1]]
+                ZPrime = [labels[x] for x in indices[1]]
+
+                avg += make_predictions(X, Y, Z, ZPrime)
+
+            return avg/num_splits
+
+        else:
+            sss = ssSplit(n_splits = 1, test_size = 0.0, random_state=42)
+
+            for indices in sss.split(arrays[:(len(pos)+len(neg))], labels):
+                X = [arrays[x] for x in indices[0]]
+                Y = [labels[x] for x in indices[0]]
+                Z = arrays[(len(pos)+len(neg)):]
+
+                return make_predictions(X, Y, Z, None)
+
+        env.close()
 
 def main():
-    print "Generating Training Data...."
-    X,Y = generate_training_data(0, 33, 10)
-    print "Creating Support Vector Machine...."
-    machine = svm.SVC()
-    print "Training Support Vector Machine..."
-    machine.fit(X,Y)
-    print "Generating Test Data...."
-    Z,ZPrime = generate_training_data(34, 43, 10)
-    print "Creating Predictions...."
-    output = machine.predict(Z)
+    """
+    Handles kmer_prediction.py being run from the command line.
+    """
+    if len(sys.argv) == 6 or len(sys.argv) == 7:
+        k = int(sys.argv[1])
+        l = int(sys.argv[2])
+        reps = int(sys.argv[3])
+        pos = set_up_files(sys.argv[4])
+        neg = set_up_files(sys.argv[5])
+        if len(sys.argv) == 6:
+            output = run(k, l, reps, pos, neg, None)
+        else:
+            predict = set_up_files(sys.argv[6])
+            output = run(k, l, reps, pos, neg, predict)
+        print output
+    else:
+        output = """
+        Error: Wrong number of arguments, requires at least 5, at most 6
+        First Argument: kmer length
+        Second Argument: Minimum kmer count required for a kmer to be output
+        Third Argument: Number of times to repeat the training and testing of
+                        the model, if greater than 1 returns the average of all
+                        runs.
+        Fourth Argument: Path to directory containing fasta files for genomes
+                         positive for a phenotype.
+        Fifth Argument: Path to a directory containing fasta files negative for
+                        a phenotype.
+        Sixth Argument: (Optional) Path to a directory containing fasta files
+                        that you want to predict whether or not they are positive
+                        for the phenotype. If supplied, all the files contianed
+                        in the fourth and fifth parameter will be used to train
+                        the model. If not supplied the files contained in the
+                        fourth and fifth argument will be shuffled and 80%% will
+                        be used to train the model and the rest will be used to
+                        test the model.
 
-    count = 0
-    for i in range(len(Z)):
-        if ZPrime[i] == output[i]:
-            count += 1
-
-    ans = (count*100)/len(Z)
-    print "Percent Correct: %d" % ans
-
-
+        Example: python %s 10 5 5 /home/data/pos/ /home/data/neg/
+        Counts kmers with length 10 removing any that appear fewer than 5 times
+        then performs the training and testing of the model 5 times using the
+        fasta files contained in the positive and negative directories.
+        """ % sys.argv[0]
+        print output
 
 if __name__ == "__main__":
     main()
