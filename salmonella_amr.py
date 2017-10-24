e73eac65c1c0f564fa1cc425edeaa149fe5b0c1e
Rylan Boothman <boothmanrylan@gmail.com>
2017-10-24 10:21:25 -0600

    Now only recounts kmers when necessary

--- salmonella_amr.py
+++ salmonella_amr.py
@@ -1,12 +1,13 @@
 import os
 import sys
 from kmer_prediction import run
+from kmer_counter import count_kmers, get_counts
 
 file_path = '/home/rboothman/Data/salmonella_amr/'
 
 key = '/home/rboothman/Data/amr_sorted.csv'
 
-def setup_files(antibiotic):
+def setup_files(antibiotic, arrays):
     susceptible = []
     resistant = []
     with open(key, 'r') as f:
@@ -26,10 +27,25 @@ def setup_files(antibiotic):
 
     return susceptible, resistant
 
-def main(k, l, reps, antibiotic):
-    sus, res = setup_files(antibiotic)
-    return run(k, l, reps, sus, res, None)
+def main(k_range, l_range, reps):
+    all_files = [file_path + x  for x in os.listdir(file_path)]
+    k_range = [int(x) for x in k_range.split(',')]
+    l_range = [int(x) for x in l_range.split(',')]
+    antibiotics = ['ampicillin', 'chloramphenicol', 'gentamicin',
+                    'kanamycin', 'nalidixic acid', 'spectinomycin',
+                    'streptomycin', 'sulphonamides', 'tetracycline',
+                    'trimethoprim']
+    for l in l_range:
+        for k in k_range:
+            count_kmers(k, l, all_files, "database")
+            for antibiotic in antibiotics:
+                sus, res = setup_files(antibiotic, all_files)
+                sus = get_counts(sus, "database")
+                res = get_counts(res, "database")
+                with open('/home/rboothman/Data/lower_limits/results_salmonella/%s/l%d.txt'%(antibiotic,l), 'a') as f:
+                    output = run(k, l, reps, sus, res, None)
+                    f.write("%d,%f\n" % (k, output))
+                    print antibiotic, k, l, output
 
 if __name__ == '__main__':
-    output = main(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]), sys.argv[4])
-    print output
+    main(sys.argv[1], sys.argv[2], int(sys.argv[3]))
