83293dfeb51737da50338d513cb5360e562dbbe7
Rylan Boothman <boothmanrylan@gmail.com>
2017-11-03 10:54:03 -0600

    Gave feature selection parameters default values, added a recursive feature elimination cv method

--- feature_selection.py
+++ feature_selection.py
@@ -1,6 +1,6 @@
 from sklearn.feature_selection import VarianceThreshold, SelectKBest
-from sklearn.feature_selection import SelectPercentile, f_classif, RFE
-from sklearn.svm import SVR
+from sklearn.feature_selection import SelectPercentile, f_classif, RFE, RFECV
+from sklearn.svm import SVC
 
 def flatten(data):
     data = data.reshape(data.shape[0], data.shape[1])
@@ -10,49 +10,80 @@ def make3D(data):
     data = data.reshape(data.shape + (1,))
     return data
 
-def variance_threshold(x_train, y_train, x_test, y_test, threshold):
-    x_train = flatten(x_train)
-    x_test = flatten(x_test)
+def variance_threshold(x_train, y_train, x_test, y_test, threshold=0.16):
+    dims = len(x_train.shape)
+    if dims == 3:
+        x_train = flatten(x_train)
+        x_test = flatten(x_test)
     feature_selector = VarianceThreshold(threshold=threshold)
     x_train = feature_selector.fit_transform(x_train)
     x_test = feature_selector.transform(x_test)
-    x_train = make3D(x_train)
-    x_test = make3D(x_test)
+    if dims == 3:
+        x_train = make3D(x_train)
+        x_test = make3D(x_test)
 
     return x_train, y_train, x_test, y_test
 
 
-def select_k_best(x_train, y_train, x_test, y_test, k):
-    x_train = flatten(x_train)
-    x_test = flatten(x_test)
-    feature_selector = SelectKBest(f_classif, k=k)
+def select_k_best(x_train, y_train, x_test, y_test, score_func=f_classif, k=500):
+    dims = len(x_train.shape)
+    if dims == 3:
+        x_train = flatten(x_train)
+        x_test = flatten(x_test)
+    feature_selector = SelectKBest(score_func=score_func, k=k)
     x_train = feature_selector.fit_transform(x_train, y_train)
     x_test = feature_selector.transform(x_test)
-    x_train = make3D(x_train)
-    x_test = make3D(x_test)
+    if dims == 3:
+        x_train = make3D(x_train)
+        x_test = make3D(x_test)
 
     return x_train, y_train, x_test, y_test
 
 
-def select_percentile(x_train, y_train, x_test, y_test, percentile):
-    x_train = flatten(x_train)
-    x_test = flatten(x_test)
-    feature_selector = SelectPercentile(f_classif, percentile=percentile)
+def select_percentile(x_train, y_train, x_test, y_test, score_func=f_classif,
+                      percentile=0.5):
+    dims = len(x_train.shape)
+    if dims == 3:
+        x_train = flatten(x_train)
+        x_test = flatten(x_test)
+    feature_selector = SelectPercentile(score_func=score_func, percentile=percentile)
     x_train = feature_selector.fit_transform(x_train, y_train)
     x_test = feature_selector.transform(x_test)
-    x_train = make3D(x_train)
-    x_test = make3D(x_test)
+    if dims == 3:
+        x_train = make3D(x_train)
+        x_test = make3D(x_test)
 
     return x_train, y_train, x_test, y_test
 
-def recursive_feature_elimination(x_train, y_train, x_test, y_test, n_features):
-    x_train = flatten(x_train)
-    x_test = flatten(x_test)
-    estimator = SVR(kernel='linear')
-    feature_selector = RFE(estimator, n_features, step=1)
+
+def recursive_feature_elimination(x_train, y_train, x_test, y_test,
+                                  estimator=SVC(kernel='linear', C=1),
+                                  n_features=None, step=1):
+    dims = len(x_train.shape)
+    if dims == 3:
+        x_train = flatten(x_train)
+        x_test = flatten(x_test)
+    feature_selector = RFE(estimator, n_features, step)
     x_train = feature_selector.fit_transform(x_train, y_train)
-    x_test = feature_selector.fit(x_test, y_test)
-    x_train = make3D(x_train)
-    x_test = make3D(x_test)
+    x_test = feature_selector.transform(x_test)
+    if dims == 3:
+        x_train = make3D(x_train)
+        x_test = make3D(x_test)
+
+    return x_train, y_train, x_test, y_test
+
+def recursive_feature_elimination_cv(x_train, y_train, x_test, y_test,
+                                     estimator=SVC(kernel='linear', C=1),
+                                     step=1, cv=3):
+    dims = len(x_train.shape)
+    if dims == 3:
+        x_train = flatten(x_train)
+        x_test = flatten(x_test)
+    feature_selector = RFECV(estimator, step, cv)
+    x_train = feature_selector.fit_transform(x_train, y_train)
+    x_test = feature_selector.transform(x_test)
+    if dims == 3:
+        x_train = make3D(x_train)
+        x_test = make3D(x_test)
 
     return x_train, y_train, x_test, y_test
