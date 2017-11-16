daef0c67565b3a47b5f8752cdbadd9f5fbc7cfcd
Rylan Boothman <boothmanrylan@gmail.com>
2017-11-16 09:48:56 -0700

    Better error handling; support for logscale functions

--- plot.py
+++ plot.py
@@ -5,24 +5,30 @@ import sys
 import os
 
 def get_data(filename):
-    with open(filename, 'r') as f:
-        data = f.read()
+    try:
+        with open(filename, 'r') as f:
+            data = f.read()
+    except IOError as E:
+        print E
+        return
     data = data.split('\n')
     data = [x.split(',') for x in data if x]
     x = [d[0] for d in data if d]
     try:
         y = [d[1] for d in data if d]
     except IndexError as E:
-        print filename
-        print data
+        print E
+        return
     return x,y
 
-def main(filepath, title, xlabel, ylabel, x_range, y_range, keep):
+def main(filepath, title, xlabel, ylabel, x_range, y_range, keep, log):
     plt.figure()
     if title:
         plt.title(' '.join(title))
     if x_range:
         plt.xticks(np.arange(x_range[0], x_range[1], x_range[2]))
+    if log:
+        plt.xscale('log', basex=2)
     if y_range:
         plt.yticks(np.arange(y_range[0], y_range[1], y_range[2]))
     plt.grid()
@@ -37,7 +43,8 @@ def main(filepath, title, xlabel, ylabel, x_range, y_range, keep):
         files = [filepath + x for x in os.listdir(filepath) if '.py' not in x]
     for f in files:
         data = get_data(f)
-        plt.plot(data[0], data[1], label=f.replace('.txt', '').replace(filepath, ''))
+        if data:
+            plt.plot(data[0], data[1], label=f.replace('.txt', '').replace(filepath, ''))
     plt.legend()
     plt.show()
 
@@ -65,6 +72,9 @@ if __name__ == "__main__":
                         type=float)
     parser.add_argument('--keep', '-k',
                         help='A substring that must be included in the filenames for them to be plotted')
+    parser.add_argument('--log', '-l',
+	                    help='If true the x axis is plotted in log space',
+	                    type=bool)
 
     args = parser.parse_args()
     args_dict = vars(args)
