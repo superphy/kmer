8ad2f70e661de9b631c7d367d37aa9396a0ff2fd
Rylan Boothman <boothmanrylan@gmail.com>
2017-11-29 10:26:30 -0700

    Removed unnecessary repittion

--- data.py
+++ data.py
@@ -8,9 +8,7 @@ import pandas as pd
 import csv
 import os
 import random
-
-
-filepath = '/home/rboothman/Data/ecoli/'
+import constants
 
 
 def get_kmer(kwargs, database="database", recount=False, k=7, l=13):
@@ -23,7 +21,7 @@ def get_kmer(kwargs, database="database", recount=False, k=7, l=13):
         k:          Size of kmer to be counted. Ignored if recount is false.
         l:          kmer cutoff value. Ignored if recount is false.
     Returns:
-        x_train, y_train, x_test, y_test
+        (x_train, y_train, x_test, y_test) ready to be passed to a ml model
     """
     x_train, y_train, x_test, y_test = parse_metadata(**kwargs)
 
@@ -40,7 +38,9 @@ def get_kmer(kwargs, database="database", recount=False, k=7, l=13):
     return (x_train, y_train, x_test, y_test)
 
 
-def get_genome(args, table='Data/binary_table.txt', sep=None):
+def get_genome_region(kwargs,
+                      table=constants.GENOME_REGION_TABLE,
+                      sep=None):
     """
     Parameters:
         args:    The arguments to pass to parse_metadata.
@@ -48,7 +48,7 @@ def get_genome(args, table='Data/binary_table.txt', sep=None):
     Returns:
         x_train, y_train, x_test, y_test
     """
-    labels = parse_metadata(*args)
+    labels = parse_metadata(**kwargs)
     x_train_labels = labels[0]
     y_train = labels[1]
     x_test_labels = labels[2]
@@ -75,168 +75,98 @@ def get_genome(args, table='Data/binary_table.txt', sep=None):
 
 def get_kmer_us_uk_split(database="database", recount=False, k=7, l=13):
     """
-    Parameters:
-        database:   lmdb database to store kmer counts.
-        threeD:     If True the data is made three dimensional.
-        recount:    If True the kmers are recounted.
-        k:          Size of kmer to be counted. Ignored if recount is false.
-        l:          kmer cutoff value. Ignored if recount is false.
-    Returns:
-        x_train, y_train, x_test, y_test
-
-        kmer data ready to be input into a ml model and recreate the Lupolova
-        et. al paper.
+    Wraps get_kmer to get the US/UK split dataset kmer data to recreate the
+    Lupolova et al paper.
     """
-    kwargs = {'prefix': filepath, 'suffix': '.fasta'}
-    x_train, y_train, x_test, y_test = parse_metadata(**kwargs)
-
-    if recount:
-        allfiles = x_train + x_test
-        count_kmers(k, l, allfiles, database)
-
-    x_train = get_counts(x_train, database)
-    x_train = np.asarray(x_train, dtype='float64')
-
-    x_test = get_counts(x_test, database)
-    x_test = np.asarray(x_test, dtype='float64')
-
-    return (x_train, y_train, x_test, y_test)
+    kwargs = {'prefix': constants.ECOLI,
+              'suffix': '.fasta'}
+    return get_kmer(kwargs, database, recount, k, l)
 
 
 def get_kmer_us_uk_mixed(database="database", recount=False, k=7, l=13):
     """
-    Parameters:
-        database:   lmdb database to store kmer counts.
-        threeD:     If True the data is made three dimensional.
-        recount:    If True the kmers are recounted.
-        k:          Size of kmer to be counted. Ignored if recount is false.
-        l:          kmer cutoff value. Ignored if recount is false.
-    Returns:
-        x_train, y_train, x_test, y_test
-
-        kmer data ready to be input into a ml model, with us/uk data shuffled
-        together.
+    Wraps get_kmer to get the US/UK mixed dataset kmer data to recreate the
+    Lupolova et al paper.
     """
-    kwargs = {'prefix': filepath, 'suffix': '.fasta', 'train_header': None}
-
-    x_train, y_train, x_test, y_test = parse_metadata(**kwargs)
-
-    if recount:
-        genomes = x_train + x_test
-        count_kmers(k, l, genomes, database)
-
-    x_train = get_counts(x_train, database)
-    x_train = np.asarray(x_train, dtype='float64')
-
-    x_test = get_counts(x_test, database)
-    x_test = np.asarray(x_test, dtype='float64')
-
-    return (x_train, y_train, x_test, y_test)
+    kwargs = {'prefix': constants.ECOLI,
+              'suffix': '.fasta',
+              'train_header': None}
+    return get_kmer(kwargs, database, recount, k, l)
 
 
-def get_genome_region_us_uk_mixed(table='Data/binary_table.txt', sep=None):
+def get_salmonella_kmer(antibiotic='ampicillin', database='database',
+                        recount=False, k=7, l=13):
     """
-    Parameters:
-        table:   binary_table.txt output from panseq.
-        threeD:  If True the data is made three dimensional.
-    Returns:
-        x_train, y_train, x_test, y_test
-
-        binary genome region presence absence data ready to be input into a ml
-        model, with us/uk data shuffled together.
+    Wraps get_kmer to get salmonella amr data.
     """
-    kwargs = {'prefix': filepath, 'suffix': '.fasta', 'train_header': None}
-    labels = parse_metadata(**kwargs)
-    x_train_labels = labels[0]
-    y_train = labels[1]
-    x_test_labels = labels[2]
-    y_test = labels[3]
-
-    x_train = []
-    x_test = []
-    if sep == None:
-        data = pd.read_csv(table, sep=sep, engine='python', index_col=0)
-    else:
-        data = pd.read_csv(table, sep=sep, index_col=0)
-
-    for header in x_train_labels:
-        x_train.append(data[header].tolist())
-
-    for header in x_test_labels:
-        x_test.append(data[header].tolist())
-
-    x_train = np.asarray(x_train)
-    x_test = np.asarray(x_test)
-
-    return (x_train, y_train, x_test, y_test)
+    kwargs = {'metadata': constants.SALMONELLA_METADATA,
+              'fasta_header': 'Fasta',
+              'label_header': 'AMR',
+              'train_header': None,
+              'extra_header': 'Antibiotic',
+              'extra_label': antibiotic,
+              'prefix': constants.SALMONELLA,
+              'suffix': '.fna'}
+    return get_kmer(kwargs, database, recount, k, l)
 
 
-def get_genome_region_us_uk_split(table='Data/binary_table.txt', sep=None):
+def get_omnilog_kmer(database="database", recount=False, k=7, l=13,
+                         classification_header='Host', positive_label=None):
     """
-    Parameters:
-        table:   binary_table.txt output from panseq.
-        threeD:  If True the data is made three dimensional.
-    Returns:
-        x_train, y_train, x_test, y_test
-
-        binary genome region presence absence data ready to be input into a ml
-        model to recreate the Lupoloval et. al paper.
+    Wraps get_kmer, to get kmer data for the omnilog fasta files.
     """
-    kwargs = {'prefix': filepath, 'suffix': '.fasta'}
-    labels = parse_metadata(**kwargs)
-    x_train_labels = labels[0]
-    y_train = labels[1]
-    x_test_labels = labels[2]
-    y_test = labels[3]
-
-    x_train = []
-    x_test = []
-    if sep == None:
-        data = pd.read_csv(table, sep=sep, engine='python', index_col=0)
-    else:
-        data = pd.read_csv(table, sep=sep, index_col=0)
-
-    for header in x_train_labels:
-        x_train.append(data[header].tolist())
-
-    for header in x_test_labels:
-        x_test.append(data[header].tolist())
-
-    x_train = np.asarray(x_train)
-    x_test = np.asarray(x_test)
-
-    return (x_train, y_train, x_test, y_test)
+    kwargs = {'metadata': 'Data/metadata.csv',
+              'fasta_header': 'Strain',
+              'label_header': classification_header,
+              'train_header': None,
+              'one_vs_all': positive_label,
+              'prefix': constants.OMNILOG_FASTA,
+              'suffix': '.fasta'}
+    return get_kmer(kwargs, database, recount, k, l)
 
 
-def get_genome_custom_filtered(input_table='Data/binary_table.txt',
-                               validation_table='Data/human_bovine_train_predictive.results',
+def get_genome_region_us_uk_mixed(table=constants.GENOME_REGION_TABLE,
+                                  sep=None):
+    """
+    Wraps get_genome_region to get the US/UK mixed datasets genome region data to
+    recreate the Lupolova et al paper.
+    """
+    kwargs = {'prefix': constants.ECOLI,
+              'suffix': '.fasta',
+              'train_header': None}
+    return get_genome_region(kwargs, table, sep)
+
+
+def get_genome_region_us_uk_split(table=constants.GENOME_REGION_TABLE,
+                                  sep=None):
+    """
+    Wraps get_genome_region to get the US/UK split dataset genome region data to
+    recreate the Lupolova et al paper.
+    """
+    kwargs = {'prefix': constants.ECOLI,
+              'suffix': '.fasta'}
+    return get_genome_region(kwargs, table, sep)
+
+
+def get_genome_custom_filtered(input_table=constants.GENOME_REGION_TABLE,
+                               filter_table=constants.PREDICTIVE_RESULTS,
                                sep=None,col='Ratio',cutoff=0.25,absolute=True,
-                               greater=True,
-                               args=None):
+                               greater=True,kwargs=None):
     """
     Parameters:
         input_table:        A binary_table output by panseq
-        validation_table:   A table containing all the same rows as input_table,
-                            but different columns.
-        sep:                The delimiter used in input_table and
-                            validation_table
-        col:                The name of the column in validation_table that has
-                            the values used to determine if a row will be kept.
-        cutoff:             What the values in "col" are compared to to decide
-                            if a row is kept or not.
-        absolute:           If true the absolute value of the values in "col" is
-                            used, if false the value is used as is
-        greater:            If true the value in "col" must be greater than the
-                            "cutoff" for a row to be kept, if false the value
-                            must be less than "cutoff"
-        args:               A list of arguments to be passed to parse_metadata.
+        filter_table:       A table to filter input_table by.
+        sep:                The delimiter used in both tables.
+        col:                Column name for the decision column in filter_table.
+        cutoff:             Float, what the values in col are compared to,
+        absolute:           If true the absolute value of values in col is used.
+        greater:            If true values in "col" must be greater than cutoff.
+        kwargs:             Dictionary of arguments for parse_metadata.
     Returns:
-        x_train, y_train, x_test, y_test ready to be input into a ml model, with
-        all the rows that do not satisfy the constraints removed in
-        validation_table removed.
+        x_train, y_train, x_test, y_test ready to be input into a ml model
     """
     if args:
-        labels = parse_metadata(*args)
+        labels = parse_metadata(**kwargs)
     else:
         labels = parse_metadata()
     x_train_labels = labels[0]
@@ -246,20 +176,20 @@ def get_genome_custom_filtered(input_table='Data/binary_table.txt',
 
     if sep == None:
         input_data=pd.read_csv(input_table,sep=sep,engine='python',index_col=0)
-        validation_data=pd.read_csv(validation_table,sep=sep,engine='python',
-                                    index_col=0)
+        filter_data=pd.read_csv(filter_table,sep=sep,engine='python',
+                                index_col=0)
     else:
         input_data = pd.read_csv(input_table, sep=sep, index_col=0)
-        validation_data = pd.read_csv(validation_data, sep=sep, index_col=0)
+        filter_data = pd.read_csv(filter_table, sep=sep, index_col=0)
 
     if absolute and greater:
-        data = input_data[abs(validation_data[col]) > cutoff]
+        data = input_data[abs(filter_data[col]) > cutoff]
     elif absolute and not greater:
-        data = input_data[abs(validation_data[col]) < cutoff]
+        data = input_data[abs(filter_data[col]) < cutoff]
     elif not absolute and greater:
-        data = input_data[validation_data[col] > cutoff]
+        data = input_data[filter_data[col] > cutoff]
     elif not absolute and not greater:
-        data = input_data[validation_data[col] < cutoff]
+        data = input_data[filter_data[col] < cutoff]
 
     x_train = []
     x_test = []
@@ -275,23 +205,22 @@ def get_genome_custom_filtered(input_table='Data/binary_table.txt',
 
     return (x_train, y_train, x_test, y_test)
 
-def get_genome_pre_filtered(input_table='Data/binary_table.txt',
-                               validation_table='Data/human_bovine_train_predictive.results',
-                               sep=None, count = 50,
-                               args=None):
+def get_genome_prefiltered(input_table=constants.GENOME_REGION_TABLE,
+                            filter_table=constants.PREDICTIVE_RESULTS,
+                            sep=None, count=50, args=None):
     """
     Parameters:
         input_table:        A binary_table output by panseq
-        validation_table:   A table containing all the same rows as input_table,
+        filter_table:       A table containing all the same rows as input_table,
                             but different columns.
         sep:                The delimiter used in input_table and
-                            validation_table
+                            filter_table
         count:              How many of the top rows to keep.
         args:               A list of arguments to be passed to parse_metadata.
     Returns:
         x_train, y_train, x_test, y_test ready to be input into a ml model, with
         all the rows that do not satisfy the constraints removed in
-        validation_table removed.
+        filter_table removed.
     """
     if args:
         labels = parse_metadata(*args)
@@ -304,7 +233,7 @@ def get_genome_pre_filtered(input_table='Data/binary_table.txt',
 
     if sep == None:
         input_data=pd.read_csv(input_table,sep=sep,engine='python',index_col=0)
-        validation_data=pd.read_csv(validation_table,sep=sep,engine='python',
+        validation_data=pd.read_csv(filter_table,sep=sep,engine='python',
                                     index_col=0)
     else:
         input_data = pd.read_csv(input_table, sep=sep, index_col=0)
@@ -329,8 +258,8 @@ def get_genome_pre_filtered(input_table='Data/binary_table.txt',
 
 
 def get_kmer_from_json(database='database',recount=False,k=7,l=13,
-                            path='/home/rboothman/moria/entero_db/',
-                            suffix='.fasta',key='assembly_barcode',*json):
+                       path=constants.MORIA,suffix='.fasta',
+                       key='assembly_barcode',*json):
     """
     Parameters:
         database,threeD,recount,k,l: See get_kmer_us_uk_split
@@ -423,66 +352,12 @@ def get_kmer_from_directory(database='database', recount=False, k=7, l=13,
 
     return output
 
-def get_salmonella_kmer(database='database2', recount=False, k=7, l=13,
-                        antibiotic='ampicillin'):
-    """
-    Parmeters:
-        database:   Name of the database to use.
-        recount:    Boolean, if true the kmers are recounted.
-        k:          The length of kmer to count.
-        l:          The minimum number of times a kmer must appear to be output
-        args:       Dictionary, the arguments to be passed to
-                    parse_salmonella_metadata.
-    Returns:
-        (x_train,y_train,x_test,y_test): Kmer data ready to be passed to a
-        machine learning model.
-    """
-    args = {'metadata': '/home/rboothman/PHAC/kmer/Data/amr_sorted.csv',
-            'fasta_header': 'Fasta', 'label_header': 'AMR',
-            'train_header': None, 'extra_header': 'Antibiotic',
-            'extra_label': antibiotic,
-            'prefix': '/home/rboothman/Data/salmonella_amr/', 'suffix': '.fna'}
-    x_train, y_train, x_test, y_test = parse_metadata(**args)
-
-    if recount:
-        all_files = x_train + x_test
-        count_kmers(k,l, all_files, database)
-
-    x_train = get_counts(x_train, database)
-    x_train = np.asarray(x_train, dtype='float64')
-
-    x_test = get_counts(x_test, database)
-    x_test = np.asarray(x_test, dtype='float64')
-
-    return (x_train, y_train, x_test, y_test)
-
-def get_kmer_for_omnilog(database="database", recount=False, k=7, l=13,
-                         classification_header='Host', positive_label='Human'):
-    """
-    Wrapper for get_kmer, provides the args necessary to parse the ecomnilog
-    metadata sheet.
-    """
-    kwargs = {'metadata': '/home/rboothman/Data/ecomnilog/metadata.csv',
-            'fasta_header': 'Strain',
-            'label_header': classification_header,
-            'train_header': None,
-            'one_vs_all': positive_label,
-            'prefix': '/home/rboothman/Data/ecomnilog/fasta (copy)/',
-            'suffix': '.fasta'}
-    data = get_kmer(kwargs, database, recount, k, 7)
-    return data
-
 def get_omnilog_data(kwargs, classification_header='Host', positive_label='Human'):
     """
 
     """
-    # kwargs = {'metadata': '/home/rboothman/Data/ecomnilog/metadata.csv',
-    #         'fasta_header': 'Strain',
-    #         'label_header': classification_header,
-    #         'train_header': None,
-    #         'one_vs_all': positive_label}
     input_data = list(parse_metadata(**kwargs))
-    omnilog_sheet='/home/rboothman/Data/ecomnilog/wide_format_header.txt'
+    omnilog_sheet= constants.OMNILOG_DATA
     omnilog_data = pd.read_csv(omnilog_sheet, index_col=0)
     valid_cols = [input_data[0].index(x) for x in input_data[0] if x in list(omnilog_data)]
     input_data[0] = [input_data[0][x] for x in valid_cols]
