445f846c01f3149133bcde058bcb12d7d0beac74
Rylan Boothman <boothmanrylan@gmail.com>
2017-11-29 10:24:16 -0700

    Added more metadata sheets, a file for constants and changed the way conda dependencies are stored

---
+++ constants.py
@@ -0,0 +1,18 @@
+#default filepaths to data
+SALMONELLA = '/home/rboothman/Data/salmonella_amr/'
+ECOLI = '/home/rboothman/Data/ecoli/'
+OMNILOG_FASTA = '/home/rboothman/Data/ecomnilog/fasta/'
+MORIA = '/home/rboothman/moria/enterobase_db/'
+GENOME_REGIONS = '/home/rboothman/Data/genome_regions/binary_tables/'
+
+#default genome_region table
+GENOME_REGION_TABLE = '/home/rboothman/Data/genome_regions/binary_tables/binary_table.txt'
+
+#omnilog data file
+OMNILOG_DATA = '/home/rboothman/Data/ecomnilog/wide_format_header.txt'
+
+#default filepaths to metadata sheets
+SALMONELLA_METADATA = './Data/amr_sorted.csv'
+ECOLI_METADATA = './Data/human_bovine.csv'
+OMNILOG_METADATA= './Data/omnilog_metadata.csv'
+PREDICTIVE_RESULTS = './Data/hb_train_predictiveresults.csv'
