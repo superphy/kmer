7154a1c8e1d8270ad1a71adca98356bc37264fd6
Rylan Boothman <boothmanrylan@gmail.com>
2017-11-29 10:25:13 -0700

    Removed old files

--- plot.sh
+++
@@ -1,5 +0,0 @@
-for f in ampicillin chloramphenicol gentamicin kanamycin nalidixic\ acid spectinomycin streptomycin sulphonamides tetracycline trimethoprim
-do
-	python plot.py "-f" /home/rboothman/Data/lower_limits/results_salmonella/$f/ "-t" kmer Length and Cutoff "for" $f -xl kmer Length -yl Accuracy -xr 0 32 1 -yr 0.65 0.97 0.02 -ey 1
-done
-
