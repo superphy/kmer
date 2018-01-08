Minor changesand validate status now passed through all stages of run so it can't be set differently for different methods and fixed issue causing non-validation runs with non-numerical class values to fail

Tests/test_data.py
@@ -219,13 +219,13 @@ class ExtractFeaturesKmer(unittest.TestCase):
         self.metadata = self.dir + 'TEMPmetadata'
         self.fasta = self.dir + 'TEMP'
         file_header = 'Fasta,Class,Dataset\n'
-        file_contents = 'TEMP,1,Train\n'
+        file_contents = 'TEMP,A,Train\n'
         with open(self.metadata, 'w') as f:
             f.write('%s%s' % (file_header, file_contents))
         with open(self.fasta, 'w') as f:
             f.write('>label1\nAAAA\n>label2\nCCCC\n>label3\nATAT\n>label4\nACGT')
         kwargs = {'metadata':self.metadata, 'prefix':self.dir, 'validate':False}
-        self.data = get_kmer(kwargs,self.db,recount=True,k=4,l=0)
+        self.data = get_kmer(kwargs,self.db,recount=True,k=4,l=0,validate=False)
         self.correct_features = np.array(['AAAA', 'ACGT', 'ATAT', 'CCCC'])
 
     def tearDown(self):
@@ -244,11 +244,11 @@ class ExtractFeaturesGenomeRegions(unittest.TestCase):
         self.table = self.dir + 'TEMPTable'
         self.metadata = self.dir + 'TEMPMetadata'
         with open(self.metadata, 'w') as f:
-            f.write('Fasta,Class,Dataset\nTEMP,1,Train')
+            f.write('Fasta,Class,Dataset\nTEMP,A,Train')
         with open(self.table, 'w') as f:
             f.write(',TEMP\nA,1\nB,1\nC,0\nD,1\n')
         kwargs = {'metadata': self.metadata, 'validate':False}
-        self.data=get_genome_regions(kwargs,table=self.table,sep=None)
+        self.data=get_genome_regions(kwargs,table=self.table,sep=None,validate=False)
         self.correct_features = np.array(['A','B','C','D'])
 
     def tearDown(self):

get_data.py
@@ -13,7 +13,8 @@ import pandas as pd
 import constants
 
 
-def get_kmer(kwargs=None, database=constants.DB, recount=False, k=7, l=13):
+def get_kmer(kwargs=None, database=constants.DB, recount=False, k=7, l=13,
+             validate=True):
     """
     Get kmer data for genomes specified in kwargs, uses kmer_counter and
     utils.parse_metadata
@@ -33,6 +34,7 @@ def get_kmer(kwargs=None, database=constants.DB, recount=False, k=7, l=13):
     """
 
     kwargs = kwargs or {}
+    kwargs['validate'] = validate
 
     (x_train, y_train, x_test, y_test) = parse_metadata(**kwargs)
 
@@ -53,7 +55,8 @@ def get_kmer(kwargs=None, database=constants.DB, recount=False, k=7, l=13):
     all_labels = np.unique(np.concatenate((y_train, y_test)))
     le.fit(all_labels)
     y_train = le.transform(y_train)
-    y_test = le.transform(y_test)
+    if validate:
+        y_test = le.transform(y_test)
 
     output_data = (x_train, y_train, x_test, y_test)
 
@@ -61,7 +64,7 @@ def get_kmer(kwargs=None, database=constants.DB, recount=False, k=7, l=13):
 
 
 def get_genome_regions(kwargs=None, table=constants.GENOME_REGION_TABLE,
-                       sep=None):
+                       sep=None, validate=True):
     """
     Gets genome region presence absence data from a binary table output by
     Panseq for the genomes specified by kwargs. Uses utils.parse_metadata
@@ -76,6 +79,7 @@ def get_genome_regions(kwargs=None, table=constants.GENOME_REGION_TABLE,
                 LabelEncoder
     """
     kwargs = kwargs or {}
+    kwargs['validate'] = validate
 
     (train_label, y_train, test_label, y_test) = parse_metadata(**kwargs)
 
@@ -100,7 +104,8 @@ def get_genome_regions(kwargs=None, table=constants.GENOME_REGION_TABLE,
     le = LabelEncoder()
     le.fit(np.concatenate((y_train, y_test)))
     y_train = le.transform(y_train)
-    y_test = le.transform(y_test)
+    if validate:
+        y_test = le.transform(y_test)
 
     output_data = (x_train, y_train, x_test, y_test)
 
@@ -124,7 +129,7 @@ def get_kmer_us_uk_split(database=constants.DB, recount=False, k=7, l=13):
     kwargs = {'prefix': constants.ECOLI,
               'suffix': '.fasta',
               'validate': True}
-    return get_kmer(kwargs, database, recount, k, l)
+    return get_kmer(kwargs, database, recount, k, l, validate=True)
 
 
 def get_kmer_us_uk_mixed(database=constants.DB, recount=False, k=7, l=13):
@@ -145,7 +150,7 @@ def get_kmer_us_uk_mixed(database=constants.DB, recount=False, k=7, l=13):
               'suffix': '.fasta',
               'train_header': None,
               'validate': True}
-    return get_kmer(kwargs, database, recount, k, l)
+    return get_kmer(kwargs, database, recount, k, l, validate=True)
 
 
 def get_salmonella_kmer(antibiotic='ampicillin', database=constants.DB,
@@ -173,7 +178,7 @@ def get_salmonella_kmer(antibiotic='ampicillin', database=constants.DB,
               'prefix': constants.SALMONELLA,
               'suffix': '.fna',
               'validate': True}
-    return get_kmer(kwargs, database, recount, k, l)
+    return get_kmer(kwargs, database, recount, k, l, validate=True)
 
 
 def get_genome_region_us_uk_mixed(table=constants.GENOME_REGION_TABLE,
@@ -193,7 +198,7 @@ def get_genome_region_us_uk_mixed(table=constants.GENOME_REGION_TABLE,
               'suffix': '.fasta',
               'train_header': None,
               'validate': True}
-    return get_genome_regions(kwargs, table, sep)
+    return get_genome_regions(kwargs, table, sep, validate=True)
 
 
 def get_genome_region_us_uk_split(table=constants.GENOME_REGION_TABLE,
@@ -212,7 +217,7 @@ def get_genome_region_us_uk_split(table=constants.GENOME_REGION_TABLE,
     kwargs = {'prefix': constants.ECOLI,
               'suffix': '.fasta',
               'validate': True}
-    return get_genome_regions(kwargs, table, sep)
+    return get_genome_regions(kwargs, table, sep, validate=True)
 
 
 def get_genome_custom_filtered(input_table=constants.GENOME_REGION_TABLE,

models.py
@@ -10,7 +10,7 @@ from keras.layers import Dense, Flatten
 from keras.models import Sequential
 from keras.layers.convolutional import Conv1D
 from keras.utils import to_categorical
-from utils import flatten, make3D, convert_to_numerical_classes
+from utils import flatten, make3D
 
 
 def neural_network(input_data, validate=True):
@@ -22,15 +22,11 @@ def neural_network(input_data, validate=True):
     probability of the prediction being correct.
     If args is not provided, default values will be used.
     """
-    input_data = input_data[:-1]
-    # TODO: No longer invert label encodings in convert_to_numerical_classes
-    input_data, le = convert_to_numerical_classes(input_data)
-
 
     x_train = input_data[0]
     y_train = input_data[1]
     x_test = input_data[2]
-    y_test = inpout_data[3]
+    y_test = input_data[3]
 
     train_classes = np.unique(np.asarray(y_train))
     num_classes = len(list(train_classes))

run.py
@@ -28,9 +28,10 @@ def run(model=models.support_vector_machine, model_args=None,
 
     Args:
         model (function):       The machine learning model to be used, see
-                                best_models.py.
-        model_args (dict):      The arguments to be passed to the model method.
-        data_method (function): The method used to gather the data, see data.py
+                                models.py.
+        model_args (dict):      The arguments to be passed to the model.
+        data_method (function): The method used to gather the data, see
+                                get_data.py
         data_args (dict):       The arguments to be passed to the data method
         scaler (function):      The method used to scale the data see
                                 feature_scaling.py.
@@ -43,14 +44,11 @@ def run(model=models.support_vector_machine, model_args=None,
                                 see data_augmentation.py
         augment_args (dict):    The arguments to be passed to the augment
                                 method.
-        validate (bool):        If true "data" should return x_train, y_train,
-                                x_test, and y_test and "model" should accept the
-                                output of data and return an accuracy. If false
-                                "data" should return x_train, y_train, and
-                                x_test and "model" should accept the output of
-                                "data" and return predictions for x_test.
-        reps (int):             How many times to run the model, if doing
-                                validation
+        validate (bool):        If true the model will return an accuracy. If
+                                false the model will return a dictionary of
+                                inputs and their predictions.
+        reps (int):             How many times to run the model, used only when
+                                doing validation otherwise set to 1.
 
     Returns:
         (dict):   Contains all of the arguments and results from the run.
@@ -73,7 +71,7 @@ def run(model=models.support_vector_machine, model_args=None,
     train_sizes = np.zeros(reps)
     test_sizes = np.zeros(reps)
 
-    data_args['kwargs']['validate'] = validate
+    data_args['validate'] = validate
     model_args['validate'] = validate
 
     all_features = []
@@ -212,6 +210,7 @@ def convert_yaml(input_file):
     output = convert_methods(input_dictionary)
     return output
 
+
 def create_arg_parser():
     """
     Creates a namespace object for the command line arguments of run.
@@ -240,12 +239,16 @@ def create_arg_parser():
     return parser.parse_args()
 
 
-if __name__ == "__main__":
-    cl_args = create_arg_parser()
-    args = convert_yaml(cl_args.input)
+def main(input_yaml, output_yaml, name):
+    args = convert_yaml(input_yaml)
     run_output = run(**args)
-    document = {'name':cl_args.name, 'output':run_output}
-    with open(cl_args.output, 'a') as output_file:
+    document = {'name':name, 'output':run_output}
+    with open(output_yaml, 'a') as output_file:
         yaml.dump(document, output_file, explicit_start=True, explicit_end=True,
                   default_flow_style=False, allow_unicode=True)
         output_file.write('\n\n\n')
+
+
+if __name__ == "__main__":
+    args = create_arg_parser()
+    main(args.input, args.output, args.name)

utils.py
@@ -9,7 +9,7 @@ import re
 import pandas as pd
 import numpy as np
 import constants
-from sklearn.preprocessing import LabelEncoder
+from Bio import SeqIO
 
 
 def setup_files(filepath):
@@ -39,8 +39,9 @@ def check_fasta(filename):
         bool: True if file is fasta|q otherwise False.
     """
     with open(filename, 'r') as f:
-        firstline = f.readline()
-        return bool(firstline[0] == '>' or firstline[0] == '@')
+        fasta = SeqIO.parse(f, "fasta")
+        fastq = SeqIO.parse(f, "fastq")
+        return bool(any(fasta) or any(fastq))
 
 
 def valid_file(test_files, *invalid_files):
@@ -121,7 +122,8 @@ def shuffle(data, labels):
 
 def flatten(data):
     """
-    Takes a 3D numpy ndarray and makes it 2D
+    Takes a 3D numpy ndarray and makes it 2D, assumes that the array was made 3D
+    using utils.make3D
 
     Args:
         data (ndarray): Numpy array to flatten.
@@ -226,7 +228,7 @@ def parse_metadata(metadata=constants.ECOLI_METADATA, fasta_header='Fasta',
         blacklist (list(str)):  A list of genome names to remove.
 
     Returns:
-        tuple: x_train, y_train, x_test, y_test; x_train and x_test contain
+        tuple: (x_train, y_train, x_test, y_test); x_train and x_test contain
                filenames, not the actual data to be passed to a machine learning
                model.
     """
@@ -248,7 +250,7 @@ def parse_metadata(metadata=constants.ECOLI_METADATA, fasta_header='Fasta',
         data[label_header] = data[label_header].where(data[label_header] == one_vs_all, 'Other')
 
     all_labels = np.unique(data[label_header])
-    all_labels = all_labels[~pd.isnull(all_labels)]
+    all_labels = all_labels[pd.notnull(all_labels)]
 
     if train_header:
         train_data = data[data[train_header] == train_label]
@@ -317,29 +319,6 @@ def parse_json(json_files, path=constants.MORIA, suffix='.fasta',
         output.append(fasta_names)
 
     return output
-
-
-def convert_to_numerical_classes(data):
-    """
-    Uses a scikitlearn LabelEncoder to convert y_train and y_test (if it exists)
-    to numerical labels, returns data as well as the label encoder to allow the
-    labels to be converted back.
-
-    Args:
-        tuple: x_train, y_train, x_test, y_test
-
-    Returns:
-        tuple: (x_train, y_train, x_test, y_test), LabelEncoder object
-    """
-    le = LabelEncoder()
-    if len(data) > 3:
-        labels = data[1]+data[3]
-        le.fit(labels)
-        output_data = (data[0], le.transform(data[1]), data[2], le.transform(data[3]))
-    else:
-        le.fit(data[1])
-        output_data = (data[0], le.transform(data[1]), data[2])
-    return output_data, le
 
 
 def convert_well_index(well_index):