8a76a5d2eb46dcacda02d115bb0270139d9c8cc3
Rylan Boothman <boothmanrylan@gmail.com>
2018-01-19 14:51:44 -0700

    Add more tests for run.py and the examples in README

---
+++ Tests/test_readme_examples.py
@@ -0,0 +1,107 @@
+import unittest
+import tempfile
+import shutil
+import yaml
+
+
+class FirstExample(unittest.TestCase):
+    def setUp(self):
+        self.dir = tempfile.mkdtemp() + '/'
+        self.config = {'model': 'neural_network',
+                       'data_method': 'get_kmer_us_uk_split',
+                       'validate': False}
+        self.config_file = self.dir + 'config_file.yml'
+        with open(self.config_file, 'w') as f:
+            yaml.dump(self.config, f)
+        self.output_file = self.dir + 'output_file.yml'
+
+    def tearDown(self):
+        shutil.rmtree(self.dir)
+
+    def test_import(self):
+        error_msg = ''
+        try:
+            from run import main
+        except ImportError as e:
+            error_msg = e
+        self.assertEqual(error_msg, '', msg=error_msg)
+
+    def test_main(self):
+        error_msg = ''
+        try:
+            from run import main
+            main(self.config_file, self.output_file, 'name of run')
+        except Exception as e:
+            error_msg = ''
+        self.assertEqual(error_msg, '', msg=error_msg)
+
+
+class SecondExample(unittest.TestCase):
+    def test_import(self):
+        error_msg = ''
+        try:
+            from run import run
+        except ImportError as e:
+            error_msg = e
+        self.assertEqual(error_msg, '', msg=error_msg)
+
+    def test_run(self):
+        error_msg = ''
+        try:
+            from run import run
+            output = run()
+        except Exception as e:
+            error_msg = ''
+        self.assertEqual(error_msg, '', msg=error_msg)
+
+
+class ThirdExample(unittest.TestCase):
+    def test_import(self):
+        error_msg = ''
+        try:
+            from models import neural_network as nn
+            from get_data import get_genome_region_us_uk_split as data
+            from feature_selection import variance_threshold as sel
+            from run import run
+        except ImportError as e:
+            error_msg = e
+        self.assertEqual(error_msg, '', msg=error_msg)
+
+    def test_run(self):
+        error_msg = ''
+        try:
+            from models import neural_network as nn
+            from get_data import get_genome_region_us_uk_split as data
+            from feature_selection import variance_threshold as sel
+            from run import run
+            output = run(model=nn, data_method=data, selection=sel,
+                         selection_args={'threshold': 0.01}, scaler=None,
+                         reps=1, validate=True)
+        except Exception as e:
+            error_msg = ''
+        self.assertEqual(error_msg, '', msg=error_msg)
+
+
+class FourthExample(unittest.TestCase):
+    def test_import(self):
+        error_msg = ''
+        try:
+            from models import neural_network
+            from get_data import get_genome_region_us_uk_mixed as data
+            from feature_selection import variance_threshold as sel
+        except ImportError as e:
+            error_msg = e
+        self.assertEqual(error_msg, '', msg=error_msg)
+
+    def test_run(self):
+        error_msg = ''
+        try:
+            from models import neural_network
+            from get_data import get_genome_region_us_uk_mixed as data
+            from feature_selection import variance_threshold as sel
+            d = data()
+            d, f = sel(d[0], d[1], threshold=0.01)
+            score = neural_network(d)
+        except Exception as e:
+            error_msg = e
+        self.assertEqual(error_msg, '', msg=error_msg)
--- Tests/test_run.py
+++ Tests/test_run.py
@@ -6,6 +6,7 @@ import tempfile
 import yaml
 from random import randint
 import numpy as np
+from run import main
 
 
 class CommandLineVariation1(unittest.TestCase):
@@ -68,15 +69,17 @@ class CommandLineVariation1(unittest.TestCase):
             args = ['python', command, '-i', self.config_file, '-o',
                     self.results_file]
             self.output = subprocess.check_output(args)
-        except subprocess.CalledProcessError:
+            self.exception_message = ''
+        except subprocess.CalledProcessError as e:
             self.output = None
+            self.esception_message = e
 
     def tearDown(self):
         shutil.rmtree(self.dir)
         shutil.rmtree(self.fasta_dir)
 
     def test_output(self):
-        self.assertIsNotNone(self.output)
+        self.assertIsNotNone(self.output, msg=self.exception_message)
         with open(self.results_file, 'r') as f:
             data = yaml.load(f)
 
@@ -85,15 +88,10 @@ class CommandLineVariation1(unittest.TestCase):
         v = {}
         v['reps'] = A(data['output']['repetitions'], self.reps)
         v['results_length'] = A(len(data['output']['results']), self.reps)
-        v['std_results'] = A(data['output']['std_dev_results'],
-                             np.asarray(data['output']['results']).std())
-        v['avg_results'] = A(data['output']['avg_result'],
-                             np.asarray(data['output']['results']).mean())
-        v['test_sizes'] = A(data['output']['test_sizes'],
-                            (self.samples - self.train_size))
-        v['train_sizes'] = A(data['output']['train_sizes'],
-                             (self.train_size +
-                             (self.add_samples * self.classes)))
+        v['std_results'] = A(data['output']['std_dev_results'], np.asarray(data['output']['results']).std())
+        v['avg_results'] = A(data['output']['avg_result'], np.asarray(data['output']['results']).mean())
+        v['test_sizes'] = A(data['output']['test_sizes'], (self.samples - self.train_size))
+        v['train_sizes'] = A(data['output']['train_sizes'], (self.train_size + (self.add_samples * self.classes)))
         vals = [x[0] for x in v.values()]
         val = False if False in vals else True
         self.assertTrue(val, msg={k: v[1:] for k, v in v.items() if not v[0]})
@@ -157,15 +155,17 @@ class CommandLineVariation2(unittest.TestCase):
             args = ['python', command, '-i', self.config_file, '-o',
                     self.results_file]
             self.output = subprocess.check_output(args)
-        except subprocess.CalledProcessError:
+            self.exception_message = ''
+        except subprocess.CalledProcessError as e:
             self.output = None
+            self.exception_message = e
 
     def tearDown(self):
         shutil.rmtree(self.dir)
         shutil.rmtree(self.fasta_dir)
 
     def test_output(self):
-        self.assertIsNotNone(self.output)
+        self.assertIsNotNone(self.output, msg=self.exception_message)
         with open(self.results_file, 'r') as f:
             data = yaml.load(f)
 
@@ -175,26 +175,174 @@ class CommandLineVariation2(unittest.TestCase):
         v['results_length'] = A(len(data['output']['results']),
                                 self.samples - self.train_size)
         v['results_type'] = A(type(data['output']['results']), dict)
-        v['test_sizes'] = A(data['output']['test_sizes'],
-                            (self.samples - self.train_size))
-        v['train_sizes'] = A(data['output']['train_sizes'],
-                             (self.train_size +
-                             (self.add_samples * self.classes)))
+        v['test_sizes'] = A(data['output']['test_sizes'], (self.samples - self.train_size))
+        v['train_sizes'] = A(data['output']['train_sizes'],  (self.train_size + (self.add_samples * self.classes)))
         vals = [x[0] for x in v.values()]
         val = False if False in vals else True
         self.assertTrue(val, msg={k: v[1:] for k, v in v.items() if not v[0]})
 
 
-# TODO: Write this test
-class ScriptVariation(unittest.TestCase):
+class ScriptVariation1(unittest.TestCase):
     def setUp(self):
-        pass
+        self.dir = tempfile.mkdtemp() + '/'
+        self.db = self.dir + 'TEMPDB'
+        self.config_file = self.dir + 'config.yml'
+        self.fasta_dir = tempfile.mkdtemp() + '/'
+        self.metadata = self.dir + 'metadata'
+        self.results_file = self.dir + 'results.txt'
+        self.samples = 25
+        self.classes = 6
+        self.train_size = 20
+        a = ['A', 'C', 'G', 'T']
+        for i in range(self.samples):
+            with open(self.fasta_dir + str(i) + '.fasta', 'w') as f:
+                f.write('>%d.fastaNODE_1\n')
+                fasta = [a[randint(0, 3)] for _ in range(6000)]
+                fasta = ''.join(fasta)
+                f.write("%s\n" % fasta)
+                f.write('>%d.fastaNODE_2\n')
+                fasta = [a[randint(0, 3)] for _ in range(7000)]
+                fasta = ''.join(fasta)
+                f.write("%s" % fasta)
+        with open(self.metadata, 'w') as f:
+            f.write('Fasta,Class,Dataset\n')
+            for i in range(self.samples):
+                f.write('%d,' % i)
+                if i < self.train_size:
+                    f.write('%d,Train\n' % (i % self.classes))
+                else:
+                    f.write(',Test\n')
+        self.config = {'model': 'random_forest',
+                       'model_args': {'validate': False},
+                       'data_method': 'get_kmer',
+                       'data_args': {'kwargs': {'metadata': self.metadata,
+                                                'prefix': self.fasta_dir,
+                                                'suffix': '.fasta',
+                                                'validate': True,
+                                                'train_header': 'Dataset',
+                                                'train_label': 'Train',
+                                                'test_label': 'Test'},
+                                     'database': self.db,
+                                     'recount': True,
+                                     'verbose': False,
+                                     'k': 5,
+                                     'L': 1},
+                       'validate': False}
+        with open(self.config_file, 'w') as f:
+            yaml.dump(self.config, f)
+        self.output = True
+        try:
+            main(self.config_file, self.results_file, '')
+            self.exception_message = ''
+        except Exception as e:
+            self.output = None
+            self.exception_message = e
 
     def tearDown(self):
-        pass
+        shutil.rmtree(self.dir)
+        shutil.rmtree(self.fasta_dir)
 
     def test_output(self):
-        self.assertTrue(1)
+        self.assertIsNotNone(self.output, msg=self.exception_message)
+        with open(self.results_file, 'r') as f:
+            data = yaml.load(f)
+
+        def A(x, y):
+            return [x == y, x, y]
+        v = {}
+        v['results_length'] = A(len(data['output']['results']),
+                                self.samples - self.train_size)
+        v['results_type'] = A(type(data['output']['results']), dict)
+        v['test_sizes'] = A(data['output']['test_sizes'], (self.samples - self.train_size))
+        v['train_sizes'] = A(data['output']['train_sizes'], (self.train_size))
+        vals = [x[0] for x in v.values()]
+        val = False if False in vals else True
+        self.assertTrue(val, msg={k: v[1:] for k, v in v.items() if not v[0]})
+
+
+class ScriptVariation2(unittest.TestCase):
+    def setUp(self):
+        self.dir = tempfile.mkdtemp() + '/'
+        self.db = self.dir + 'TEMPDB'
+        self.config_file = self.dir + 'config.yml'
+        self.fasta_dir = tempfile.mkdtemp() + '/'
+        self.metadata = self.dir + 'metadata'
+        self.results_file = self.dir + 'results.txt'
+        self.reps = 1
+        self.samples = 30
+        self.classes = 3
+        self.add_samples = 4
+        self.train_size = 15
+        a = ['A', 'C', 'G', 'T']
+        for i in range(self.samples):
+            with open(self.fasta_dir + str(i) + '.fasta', 'w') as f:
+                f.write('>%d.fastaNODE_1\n')
+                fasta = [a[randint(0, 3)] for _ in range(500)]
+                fasta = ''.join(fasta)
+                f.write("%s\n" % fasta)
+                f.write('>%d.fastaNODE_2\n')
+                fasta = [a[randint(0, 3)] for _ in range(500)]
+                fasta = ''.join(fasta)
+                f.write("%s" % fasta)
+        with open(self.metadata, 'w') as f:
+            f.write('Fasta,Class,Dataset\n')
+            for i in range(self.samples):
+                f.write('%d,' % i)
+                f.write('%d,' % (i % self.classes))
+                if i < self.train_size:
+                    f.write('Train\n')
+                else:
+                    f.write('Test\n')
+        self.config = {'model': 'support_vector_machine',
+                       'model_args': {'validate': True},
+                       'data_method': 'get_kmer',
+                       'data_args': {'kwargs': {'metadata': self.metadata,
+                                                'prefix': self.fasta_dir,
+                                                'suffix': '.fasta'},
+                                     'database': self.db,
+                                     'recount': True,
+                                     'verbose': False,
+                                     'k': 4,
+                                     'L': 1},
+                       'scaler': 'scale_to_range',
+                       'scaler_args': {'low': 0,
+                                       'high': 4},
+                       'selection': 'select_k_best',
+                       'selection_args': {'score_func': 'f_classif',
+                                          'k': 15},
+                       'augment': 'augment_data_noise',
+                       'augment_args': {'desired_samples': self.add_samples},
+                       'validate': True,
+                       'reps': self.reps}
+        with open(self.config_file, 'w') as f:
+            yaml.dump(self.config, f)
+        try:
+            main(self.config_file, self.results_file, 'Run1')
+            self.exception_message = ''
+        except Exception as e:
+            self.exception_message = e
+
+    def tearDown(self):
+        shutil.rmtree(self.dir)
+        shutil.rmtree(self.fasta_dir)
+
+    def test_output(self):
+        self.assertEqual(self.exception_message, '', msg=self.exception_message)
+        with open(self.results_file, 'r') as f:
+            data = yaml.load(f)
+
+        def A(x, y):
+            return [x == y, x, y]
+        v = {}
+        v['reps'] = A(data['output']['repetitions'], self.reps)
+        v['results_length'] = A(len(data['output']['results']), self.reps)
+        v['std_results'] = A(data['output']['std_dev_results'], np.asarray(data['output']['results']).std())
+        v['avg_results'] = A(data['output']['avg_result'], np.asarray(data['output']['results']).mean())
+        v['test_sizes'] = A(data['output']['test_sizes'], (self.samples - self.train_size))
+        v['train_sizes'] = A(data['output']['train_sizes'], (self.train_size + (self.add_samples * self.classes)))
+        vals = [x[0] for x in v.values()]
+        val = False if False in vals else True
+        self.assertTrue(val, msg={k: v[1:] for k, v in v.items() if not v[0]})
 
 
 if __name__ == "__main__":
