1fd3058430e6b4d5bdc15e52e27b42cfadcd0fb8
Rylan Boothman <boothmanrylan@gmail.com>
2018-02-26 10:26:58 -0700

    Check that genereate_config has been run before running run_all

--- analysis/run_all.py
+++ analysis/run_all.py
@@ -1,14 +1,27 @@
+"""
+Recreates the analysis from the paper
+"""
 import yaml
 import os
 import sys
 sys.path.append('../../')
 from run import main
+from generate_config import generate_config_files
 
-all_files = [x for x in os.listdir('./config_files/') if '.yml' in x]
+def run_all():
+    directory = './config_files/'
+    results_dir = './results/'
 
-for f in all_files:
-    main(f, './results/omnilog/%s' % f, f.replace('.yml', ''))
+    if not os.exists(directory):
+        generate_config_files()
 
+    if not os.exists(results_dir):
+        os.makedirs(results_dir)
 
+    all_files = [x for x in os.listdir(directory) if '.yml' in x]
 
+    for f in all_files:
+        main(f, results_dir + '%s' % f, f.replace('.yml', ''))
 
+if __name__ == '__main__':
+    run_all()
