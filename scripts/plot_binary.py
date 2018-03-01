a08c9b88e6a02f6c0c40a36c2b23e0c0b525994d
Rylan Boothman <boothmanrylan@gmail.com>
2018-03-01 14:18:47 -0700

    Fix correctly identify snakemake.wildcards

--- scripts/plot_binary.py
+++ scripts/plot_binary.py
@@ -14,7 +14,7 @@ hue_order = np.unique(df['Data Type'].values)
 
 sns.set(font_scale=1.5)
 
-g = sns.factorplot(x=wildcards.prediction, y='Accuracy', hue='Data Type',
+g = sns.factorplot(x=snakemake.wildcards.prediction, y='Accuracy', hue='Data Type',
                   col='Model Type', kind='box', data=df, legend=False,
                   col_order=col_order, hue_order=hue_order, size=12,
                   fliersize=10, linewidth=2,
@@ -23,6 +23,6 @@ g = sns.factorplot(x=wildcards.prediction, y='Accuracy', hue='Data Type',
 g.set_titles("Using a {col_name}")
 g.axes[0][0].legend(loc='lower left', title='Data Type')
 plt.subplots_adjust(top=0.9)
-g.fig.suptitle("Accuracy When Making Binary {} Predictions".format(wildcards.prediction))
+g.fig.suptitle("Accuracy When Making Binary {} Predictions".format(snakemake.wildcards.prediction))
 
 plt.savefig(snakemake.output[0], dpi=1200)
