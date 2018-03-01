2d3703c9338c8921a18a023d008f983f39762b26
Rylan Boothman <boothmanrylan@gmail.com>
2018-03-01 14:19:06 -0700

    Remove col_order and hue_order

--- scripts/plot_us_uk.py
+++ scripts/plot_us_uk.py
@@ -9,10 +9,11 @@ import numpy as np
 
 sns.set(font_scale=1.5)
 
+df = pd.read_csv(snakemake.input[0])
+
 g = sns.factorplot(x='Dataset', y='Accuracy', hue='Data Type',
                   col='Model Type', kind='box', data=df, legend=False,
-                  col_order=col_order, hue_order=hue_order, size=12,
-                  fliersize=10, linewidth=2,
+                  size=12, fliersize=10, linewidth=2,
                   medianprops={'color': 'black', 'linewidth': 3,
                                 'solid_capstyle': 'butt'})
 g.set_titles("Using a {col_name}")
