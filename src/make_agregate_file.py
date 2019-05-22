386372836bc8b3c4d75d3c094e1eb97ed2179db4
Gates Kempenaar <kempenaarg@gmail.com>
2019-05-22 15:48:22 -0600

    reformats omnilog csv files

--- src/make_agregate_file.py
+++ src/make_agregate_file.py
@@ -3,28 +3,94 @@ import os
 import numpy as np
 
 path = os.path.abspath('../../Desktop/ecoli/biolog_csv/_07-2890_Escherichia coli__2_37_PMX_438_2#19#2019_A_ 1B_1.csv')
-path2 = os.path.abspath('../../Desktop/ecoli/PM21/_07-2890_Escherichia coli__2_37_PMX_438_2#19#2019_A_ 1B_1.csv')
 
 new_data = pd.read_csv(path)
-new_data2 = pd.read_csv(path2)
 
 data_file = new_data["Data File"]
 file_name = data_file[1]
 
 setup_time = new_data["Setup Time"]
+time = setup_time[1]
+
 position = new_data["Position"]
+pos = position[1]
+
 plate_type = new_data["Plate Type"]
+plate = plate_type[1]
+
 strain_type = new_data["Strain Type"]
+strain = strain_type[1]
+
 sample_number = new_data["Sample Number"]
+sample = sample_number[1]
+
 strain_name = new_data["Strain Name"]
+name = strain_name[1]
+
 strain_number = new_data["Strain Number"]
-other = new_data["Other"]
+number = strain_number[1]
+
+other_info = new_data["Other"]
+other = other_info[1]
+
+# get areas under the curve
+hour = new_data.loc[:3000, 'Hour':]
 
 
 list = []
-for x in range(0, 300):
+for x in range(0, 9):
     list.append(x)
 
 df = pd.DataFrame(index = list, columns = ['Data File    ', '{0}'.format(file_name), 'Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4', 'Unnamed: 5', 'Unnamed: 6', 'Unnamed: 7', 'Unnamed: 8', 'Unnamed: 9', 'Unnamed: 10', 'Unnamed: 11', 'Unnamed: 12', 'Unnamed: 13', 'Unnamed: 14', 'Unnamed: 15', 'Unnamed: 16', 'Unnamed: 17', 'Unnamed: 18', 'Unnamed: 19', 'Unnamed: 20', 'Unnamed: 21', 'Unnamed: 22', 'Unnamed: 23', 'Unnamed: 24', 'Unnamed: 25', 'Unnamed: 26', 'Unnamed: 27', 'Unnamed: 28', 'Unnamed: 29', 'Unnamed: 30', 'Unnamed: 31', 'Unnamed: 32', 'Unnamed: 33', 'Unnamed: 34', 'Unnamed: 35', 'Unnamed: 36', 'Unnamed: 37', 'Unnamed: 38', 'Unnamed: 39', 'Unnamed: 40', 'Unnamed: 41', 'Unnamed: 42', 'Unnamed: 43', 'Unnamed: 44', 'Unnamed: 45', 'Unnamed: 46', 'Unnamed: 47', 'Unnamed: 48', 'Unnamed: 49', 'Unnamed: 50', 'Unnamed: 51', 'Unnamed: 52', 'Unnamed: 53', 'Unnamed: 54', 'Unnamed: 55', 'Unnamed: 56', 'Unnamed: 57', 'Unnamed: 58', 'Unnamed: 59', 'Unnamed: 60', 'Unnamed: 61', 'Unnamed: 62', 'Unnamed: 63', 'Unnamed: 64', 'Unnamed: 65', 'Unnamed: 66', 'Unnamed: 67', 'Unnamed: 68', 'Unnamed: 69', 'Unnamed: 70', 'Unnamed: 71', 'Unnamed: 72', 'Unnamed: 73', 'Unnamed: 74', 'Unnamed: 75', 'Unnamed: 76', 'Unnamed: 77', 'Unnamed: 78', 'Unnamed: 79', 'Unnamed: 80', 'Unnamed: 81', 'Unnamed: 82', 'Unnamed: 83', 'Unnamed: 84', 'Unnamed: 85', 'Unnamed: 86', 'Unnamed: 87', 'Unnamed: 88', 'Unnamed: 89', 'Unnamed: 90', 'Unnamed: 91', 'Unnamed: 92', 'Unnamed: 93', 'Unnamed: 94', 'Unnamed: 95', 'Unnamed: 96'])
 
-print(df)
+# set time
+df.at[0, 'Data File    '] = 'Set up Time'
+df.at[0, file_name] = time
+
+# set Position
+df.at[1, 'Data File    '] = 'Position'
+df.at[1, file_name] = pos
+
+# set plate type
+df.at[2, 'Data File    '] = 'Plate Type'
+df.at[2, file_name] = plate
+
+# set strain type
+df.at[3, 'Data File    '] = 'Strain Type'
+df.at[3, file_name] = strain
+
+# set sample number
+df.at[4, 'Data File    '] = 'Sample Number'
+df.at[4, file_name] = sample
+
+# set strain name
+df.at[5, 'Data File    '] = 'Strain Name'
+df.at[5, file_name] = name
+
+# set strain number
+df.at[6, 'Data File    '] = 'Strain Number'
+df.at[6, file_name] = number
+
+# set other
+df.at[7, 'Data File    '] = 'Other'
+df.at[7, file_name] = other
+
+df0 = pd.DataFrame(hour)
+
+header_list = df0.columns.values
+values_list = df0.values.tolist()
+s = pd.Series(header_list, index=df.columns)
+
+df2 = pd.DataFrame(columns = ['Data File    ', '{0}'.format(file_name), 'Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4', 'Unnamed: 5', 'Unnamed: 6', 'Unnamed: 7', 'Unnamed: 8', 'Unnamed: 9', 'Unnamed: 10', 'Unnamed: 11', 'Unnamed: 12', 'Unnamed: 13', 'Unnamed: 14', 'Unnamed: 15', 'Unnamed: 16', 'Unnamed: 17', 'Unnamed: 18', 'Unnamed: 19', 'Unnamed: 20', 'Unnamed: 21', 'Unnamed: 22', 'Unnamed: 23', 'Unnamed: 24', 'Unnamed: 25', 'Unnamed: 26', 'Unnamed: 27', 'Unnamed: 28', 'Unnamed: 29', 'Unnamed: 30', 'Unnamed: 31', 'Unnamed: 32', 'Unnamed: 33', 'Unnamed: 34', 'Unnamed: 35', 'Unnamed: 36', 'Unnamed: 37', 'Unnamed: 38', 'Unnamed: 39', 'Unnamed: 40', 'Unnamed: 41', 'Unnamed: 42', 'Unnamed: 43', 'Unnamed: 44', 'Unnamed: 45', 'Unnamed: 46', 'Unnamed: 47', 'Unnamed: 48', 'Unnamed: 49', 'Unnamed: 50', 'Unnamed: 51', 'Unnamed: 52', 'Unnamed: 53', 'Unnamed: 54', 'Unnamed: 55', 'Unnamed: 56', 'Unnamed: 57', 'Unnamed: 58', 'Unnamed: 59', 'Unnamed: 60', 'Unnamed: 61', 'Unnamed: 62', 'Unnamed: 63', 'Unnamed: 64', 'Unnamed: 65', 'Unnamed: 66', 'Unnamed: 67', 'Unnamed: 68', 'Unnamed: 69', 'Unnamed: 70', 'Unnamed: 71', 'Unnamed: 72', 'Unnamed: 73', 'Unnamed: 74', 'Unnamed: 75', 'Unnamed: 76', 'Unnamed: 77', 'Unnamed: 78', 'Unnamed: 79', 'Unnamed: 80', 'Unnamed: 81', 'Unnamed: 82', 'Unnamed: 83', 'Unnamed: 84', 'Unnamed: 85', 'Unnamed: 86', 'Unnamed: 87', 'Unnamed: 88', 'Unnamed: 89', 'Unnamed: 90', 'Unnamed: 91', 'Unnamed: 92', 'Unnamed: 93', 'Unnamed: 94', 'Unnamed: 95', 'Unnamed: 96'])
+
+s = pd.Series(header_list, index=df2.columns)
+df2 = df2.append(s, ignore_index=True)
+
+df3 = pd.DataFrame(data = values_list, columns = ['Data File    ', '{0}'.format(file_name), 'Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4', 'Unnamed: 5', 'Unnamed: 6', 'Unnamed: 7', 'Unnamed: 8', 'Unnamed: 9', 'Unnamed: 10', 'Unnamed: 11', 'Unnamed: 12', 'Unnamed: 13', 'Unnamed: 14', 'Unnamed: 15', 'Unnamed: 16', 'Unnamed: 17', 'Unnamed: 18', 'Unnamed: 19', 'Unnamed: 20', 'Unnamed: 21', 'Unnamed: 22', 'Unnamed: 23', 'Unnamed: 24', 'Unnamed: 25', 'Unnamed: 26', 'Unnamed: 27', 'Unnamed: 28', 'Unnamed: 29', 'Unnamed: 30', 'Unnamed: 31', 'Unnamed: 32', 'Unnamed: 33', 'Unnamed: 34', 'Unnamed: 35', 'Unnamed: 36', 'Unnamed: 37', 'Unnamed: 38', 'Unnamed: 39', 'Unnamed: 40', 'Unnamed: 41', 'Unnamed: 42', 'Unnamed: 43', 'Unnamed: 44', 'Unnamed: 45', 'Unnamed: 46', 'Unnamed: 47', 'Unnamed: 48', 'Unnamed: 49', 'Unnamed: 50', 'Unnamed: 51', 'Unnamed: 52', 'Unnamed: 53', 'Unnamed: 54', 'Unnamed: 55', 'Unnamed: 56', 'Unnamed: 57', 'Unnamed: 58', 'Unnamed: 59', 'Unnamed: 60', 'Unnamed: 61', 'Unnamed: 62', 'Unnamed: 63', 'Unnamed: 64', 'Unnamed: 65', 'Unnamed: 66', 'Unnamed: 67', 'Unnamed: 68', 'Unnamed: 69', 'Unnamed: 70', 'Unnamed: 71', 'Unnamed: 72', 'Unnamed: 73', 'Unnamed: 74', 'Unnamed: 75', 'Unnamed: 76', 'Unnamed: 77', 'Unnamed: 78', 'Unnamed: 79', 'Unnamed: 80', 'Unnamed: 81', 'Unnamed: 82', 'Unnamed: 83', 'Unnamed: 84', 'Unnamed: 85', 'Unnamed: 86', 'Unnamed: 87', 'Unnamed: 88', 'Unnamed: 89', 'Unnamed: 90', 'Unnamed: 91', 'Unnamed: 92', 'Unnamed: 93', 'Unnamed: 94', 'Unnamed: 95', 'Unnamed: 96'])
+print(df3)
+
+df4 = df.append(df2)
+df5 = df4.append(df3)
+print(df5)
+
+df5.to_csv("./test.csv")
