43917f3fc847d4fc2bb039f7e5c697dc9f431d05
Gates Kempenaar <kempenaarg@gmail.com>
2019-06-03 14:12:40 -0600

    fixed file path

--- src/fix_omnilog_summary.py
+++ src/fix_omnilog_summary.py
@@ -4,7 +4,7 @@ import re
 
 pattern = re.compile(r'[P][M]\d+\s\{[a-z]*\s\=\s\"[C-z]*\s[a-z]*\s[a-z]*\"\}\s\w\d+\s')
 
-with open("data/data_summary.txt", 'r') as read, open("data/onmilog_data_summary.txt", 'w') as w:
+with open("data/data_summary.txt", 'r') as read, open("data/omnilog_data_summary.txt", 'w') as w:
     for line in read:
         if re.search(pattern, line):
             w.write(re.sub(pattern, '', line))
