dde2efc61331a377401320ccf3f0b63466d6ef97
Gates Kempenaar <kempenaarg@gmail.com>
2019-08-26 14:53:19 -0600

    distance matrix

---
+++ src/make_trees/omnilog_tree2.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from scipy.spatial.distance import squareform, pdist
+from skbio.tree import nj
+
+temp = []
+
+with open("data/omnilog_data_summary.txt", 'r') as read:
+    for line in read:
+        temp.append(line.split('\t'))
+
+for list in temp:
+    num = list[2].split('\n')
+    num = float(num[0])
+    list[2] = num
+    list.append(num)
+
+df = pd.DataFrame(data = temp, columns = ["Strain", "Substrate", "AUC", "AUC"])
+print(df.iloc[:, 2:])
+
+distance = pd.DataFrame(squareform(pdist(df.iloc[:, 2:])), columns=df.Strain.unique(), index=df.Strain.unique())
+
+tree = nj(distance)
